5701b33d113e1079b66ac617b6899a1262992ec3
mikemrg17 <miguelangelmurga1234@gmail.com>
2022-11-06 12:17:12 -0600

    Tabulacion de AFD

--- Automata/src/com/automata/ConjuntoS.java
+++ Automata/src/com/automata/ConjuntoS.java
@@ -1,14 +1,16 @@
 package com.automata;
 
+import java.util.Arrays;
 import java.util.HashSet;
 
 //Equivalente a clase ConjIJ
 public class ConjuntoS { //Esta clase sirve para poder guardar los estados del AFD
-    public static HashSet<ConjuntoS> conjuntos_s = new HashSet<ConjuntoS>();
+    private static HashSet<ConjuntoS> conjuntos_s = new HashSet<ConjuntoS>();
     public HashSet<Estado> estados = new HashSet<Estado>();
     public int id;
-    static int contador_conjuntos_s = 0;
+    public static int contador_conjuntos_s = 0;
     public boolean es_de_aceptacion = false;
+    public int[] transiciones = new int[256];
     
     //CONSTRUCTORES
     public ConjuntoS(){
@@ -16,13 +18,23 @@ public class ConjuntoS { //Esta clase sirve para poder guardar los estados del A
         estados.clear();
         es_de_aceptacion = false;
         conjuntos_s.add(this);
+        Arrays.fill(transiciones, -1);
     }
     
     public ConjuntoS(HashSet<Estado> estados){
         id = contador_conjuntos_s++;
-        estados = estados;
+        this.estados = estados;
         es_de_aceptacion = false;
         conjuntos_s.add(this);
+        Arrays.fill(transiciones, -1);
+    }
+    
+    public void SetTransicion(char simbolo, Estado estado){
+        transiciones[(int)simbolo] = estado.id;
+    }
+    
+    public void setToken(int token){
+        transiciones[255] = token;
     }
     
 }
